f4cf03cfa3262f54ca72ece1d26890afdb1291f4
radscheit <matthias@radscheit.de>
2016-01-09 23:37:27 +0100

    state before including landos frontend

--- BetterMeetingsWithReact/assets/app/app.js
+++ BetterMeetingsWithReact/assets/app/app.js
@@ -1,4 +1,7 @@
 var CONST_ES6_BUILD_PATH = './build/';
-
 var _  = require('lodash');
+var io = require('../vendor/sails.io.js/sails.io.js')();
 var React = require('react');
+$ = jQuery = require('jquery');
+var bootstrap = require('../vendor/bootstrap/dist/js/bootstrap.min.js')
+
