7b1915b2a2bfabf5502a7cfcffcc1b482799f324
Matthias Radscheit <matthias@radscheit.de>
2016-01-20 17:10:43 +0100

    added standard methods for MeetingController

--- BetterMeetingsWithReact/api/controllers/MeetingController.js
+++ BetterMeetingsWithReact/api/controllers/MeetingController.js
@@ -7,22 +7,63 @@
 
 module.exports = {
 	
-	create: function (req, res) {
-		Meeting.create(req.body).done(function(err, meeting) {
-			res.end(JSON.stringify(meeting));
-		})
-	},
+	create: function(req,res) {
+    if (req.method == "POST" && req.param( "Meeting" , null ) != null ) {
+      Meeting.create( req.param("Meeting") ).done( function( err , model ) {
+        if ( err ) {
+          res.send("Error");
+        } else {
+          res.send("Success");
+        }
+      });
+    } else {
+      res.render('meeting/view');
+    }
+  },
 
-  /**
-   * `MeetingController.createJourFixe()`
-   */
-  createJourFixe: function (req, res) {
-    return res.json({
-      todo: 'createJourFixe() is not implemented yet!'
+  update: function(req,res) {
+    var todoID = req.param("meetingID", null);
+
+    Meeting.findOne(todoID).done(function(err,model) {
+      if (req.method == "POST" && req.param("Meeting",null) != null) {
+        var item = req.param("Meeting",null);
+        model.members = item.members;
+        model.topics = item.topics;
+        model.jourFixe = item.jourFixe;
+        model.timer = item.timer;
+
+        model.save(function(err) {
+          if (err) {
+            res.send("Error");
+          } else {
+            res.send("Success");
+          }
+        });
+      } else {
+        res.render('meeting/view',{'model':model});
+      }
+    })
+  },
+
+  view: function(req,res) {
+    var todoID = req.param("meetingID", null);
+
+    Meeting.findOne(todoID).done(function(err,model) {
+      res.render('meeting/view', {'model':model});
     });
   },
 
-  generateurl: function (req, res) {
+  delete: function(req,res) {
+    var todoID = req.param("MeetingID", null);
+
+    Meeting.findOne(todoID).done(function(err,user) {
+      user.destroy(function(err) {
+        res.send("Success");
+      });
+    });
+  },
+
+  generateurl: function () {
     var s = "";
       while(s.length<8&&8>0){
         var r = Math.random();
