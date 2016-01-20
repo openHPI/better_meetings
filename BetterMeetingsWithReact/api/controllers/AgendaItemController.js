567b40afdf542bc131c4e5edd445ac151d51ee66
Matthias Radscheit <matthias@radscheit.de>
2016-01-20 17:02:01 +0100

    added standard methods to AgendaItemController

--- BetterMeetingsWithReact/api/controllers/AgendaItemController.js
+++ BetterMeetingsWithReact/api/controllers/AgendaItemController.js
@@ -7,7 +7,61 @@
 
 module.exports = {
 	
+  create: function(req,res) {
+    if (req.method == "POST" && req.param( "AgendaItem" , null ) != null ) {
+      AgendaItem.create( req.param("AgendaItem") ).done( function( err , model ) {
+        if ( err ) {
+          res.send("Error");
+        } else {
+          res.send("Success");
+        }
+      });
+    } else {
+      res.render('meeting/view');
+    }
+  },
 
+  update: function(req,res) {
+    var todoID = req.param("AgendaItemID", null);
+
+    AgendaItem.findOne(todoID).done(function(err,model) {
+      if (req.method == "POST" && req.param("AgendaItem",null) != null) {
+        var item = req.param("AgendaItem",null);
+        model.title = item.title;
+        model.description = item.description;
+        model.todos = item.todos;
+        model.done = item.done;
+
+        model.save(function(err) {
+          if (err) {
+            res.send("Error");
+          } else {
+            res.send("Success");
+          }
+        });
+      } else {
+        res.render('meeting/view',{'model':model});
+      }
+    })
+  },
+
+  view: function(req,res) {
+    var todoID = req.param("AgendaItemID", null);
+
+    AgendaItem.findOne(todoID).done(function(err,model) {
+      res.render('meeting/view', {'model':model});
+    });
+  },
+
+  delete: function(req,res) {
+    var todoID = req.param("AgendaItemID", null);
+
+    AgendaItem.findOne(todoID).done(function(err,user) {
+      user.destroy(function(err) {
+        res.send("Success");
+      });
+    });
+  },
 
   /**
    * `AgendaItemController.isDone()`
