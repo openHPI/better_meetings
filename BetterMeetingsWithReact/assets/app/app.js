bb70a3d51e7f8e66cd552cac311e613934e0454e
Matthias Radscheit <matthias@radscheit.de>
2016-01-22 20:00:41 +0100

    reenabled asyn connection to sails

--- BetterMeetingsWithReact/assets/app/app.js
+++ BetterMeetingsWithReact/assets/app/app.js
@@ -1,12 +1,25 @@
 window.React = require('react');
-global.jQuery = require("jquery");
+global.jQuery = require('jquery');
 var bootstrap = require('bootstrap');
+// var socketIO = require('../vendor/socket.io');
+// var io = require('../vendor/sails.io.js/sails.io.js')(socketIO);
+
+var socketIOClient = require('socket.io-client');
+var sailsIOClient = require('sails.io.js');
+
 var ProductData = require('./ExampleData');
 var MeetingDataAPI = require('./utils/MeetingExampleDataAPI');
 var FluxMeetingApp = require('./components/FluxMeetingApp.react');
+// var io = require('../vendor/sails.io.js/sails.io.js');
 
 // Load Mock Product Data into localStorage
 ProductData.init();
+
+var io = sailsIOClient(socketIOClient);
+io.sails.url = 'http://localhost:1337';
+
+
+
 
 // Load
 // MeetingDataAPI.subscribeAndListen();
