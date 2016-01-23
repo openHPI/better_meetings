3d6242979b9a78e1939a0e34e7140e0455ccfe22
Matthias Radscheit <matthias@radscheit.de>
2016-01-23 21:18:02 +0100

    added UrlService.js and generateURl function

---
+++ BetterMeetingsWithReact/api/services/UrlService.js
@@ -0,0 +1,18 @@
+/**
+ * UrlServive
+ *
+ */
+
+module.exports = {
+
+    generateurl: function () {
+      var s = "";
+        while(s.length<8&&8>0){
+          var r = Math.random();
+          s+= (r<0.1?Math.floor(r*100):String.fromCharCode(Math.floor(r*26) + (r>0.5?97:65)));
+        }
+      return res.json({
+        url: s,
+      })
+    },
+};
