922a82c36493edc73cdb126b0216ce56999e0958
weaslbe <ajay.kesar@student.hpi.de>
2016-01-25 14:04:18 +0100

    fixed route and exampledata

--- BetterMeetingsWithReact/config/routes.js
+++ BetterMeetingsWithReact/config/routes.js
@@ -73,10 +73,6 @@ module.exports.routes = {
     controller: 'person',
     action: 'exampledata',
   },
-  '/admin/create/' : {
-    controller: 'meetingadmin',
-    action: 'create'
-  },
 
   // Endpoints
   'post /login': 'PersonController.login',
