d80125814b400a19fd92ecc5d29760518f9cba33
Benjamin Feldmann <benjamin-feldmann@web.de>
2016-01-26 14:41:28 +0100

    add qr code service

--- BetterMeetingsWithReact/api/controllers/UserController.js
+++ BetterMeetingsWithReact/api/controllers/UserController.js
@@ -7,87 +7,90 @@
 
 module.exports = {
 
-create: function(req,res) {
+  create: function (req, res) {
 
-	var nameSent = req.param('name');
-         console.log(req.param);
-         console.log(nameSent);
+    var nameSent = req.param('name');
+    console.log(req.param);
+    console.log(nameSent);
 
-	  if (nameSent ){
+    if (nameSent) {
 
-		  User.create({name: nameSent}).exec(function createCB(err,created){
-                                    if (err) console.log('Fehler: ',err);
+      User.create({name: nameSent}).exec(function createCB(err, created) {
+        if (err) console.log('Fehler: ', err);
 
-                                    User.publishCreate({
-			  	     	id: created.id,
-			  	     	name: created.name,
-			  	     });
-		     return res.json({
-		       notice: 'Created user with name ' + created.name
-		     });
-		   });
-		} else if (req.isSocket){
+        User.publishCreate({
+          id: created.id,
+          name: created.name,
+        });
+        return res.json({
+          notice: 'Created user with name ' + created.name
+        });
+      });
+    } else if (req.isSocket) {
 
-		         User.watch(req);
-		         console.log('User with socket id ' + sails.sockets.id(req)+' is now subscribed to the model class \'users\'.');
+      User.watch(req);
+      console.log('User with socket id ' + sails.sockets.id(req) + ' is now subscribed to the model class \'users\'.');
 
-		} else {
-			res.send('view')
-		         //res.view();
+    } else {
+      res.send('view')
+      //res.view();
 
-       }
+    }
 
-	},
-	view: function(req,res) {
+  },
+  view: function (req, res) {
+    User.findOne(id).exec(function displayList(err, items) {
+      console.log(items);
+      res.response = items;
+      res.render('user', {'model': 'User'});
 
-           User.findOne(id).exec(function displayList(err, items) {
-               console.log(items);
-               res.response = items;
-               res.render('user', {'model': 'User'});
+    })
+  },
 
-          })
-         },
+  viewAll: function (req, res) {
+    var qrcode = QrCodeService.renderQrCode('http://localhost:1337/user', '250');
 
-   viewAll: function(req,res) {
+    User.find().exec(function displayList(err, items) {
+      if (err) return res.serverError(err);
 
-     User.find().exec(function displayList(err, items) {
-         if (err) return res.serverError(err);
-
-         console.log(items);
-         return res.view('user', {
-            users: items,}
-            );
+      console.log(items);
+      return res.view(
+        'user', {
+          users: items,
+          qr: qrcode,
+        }
+      );
 
     });
-   },
+  },
 
-	delete: function(req,res) {
-	  var userID = req.param("userID", null);
+  delete: function (req, res) {
+    var userID = req.param("userID", null);
 
-	  User.findOne(userID).done(function(err,user) {
-	    user.destroy(function(err) {
-	    	if (err) {
-          		sails.log('Error while deleting user');
-          		res.send("Error");
+    User.findOne(userID).done(function (err, user) {
+      user.destroy(function (err) {
+        if (err) {
+          sails.log('Error while deleting user');
+          res.send("Error");
         }
-	      res.send("Success");
-	    });
-	  });
-	},
+        res.send("Success");
+      });
+    });
+  },
 
-	displayAll: function (req,res) {
+  displayAll: function (req, res) {
 
-		User.find(function storedUsers(err,users) {
-			User.subscribe(req.socket);
-			User.subscribe(req.socket, users);
-		});
-	},
+    User.find(function storedUsers(err, users) {
+      User.subscribe(req.socket);
+      User.subscribe(req.socket, users);
+    });
+  },
 
-	subscribe: function(req,res) {
-   		if (req.isSocket) {
-      		user.watch(req);
-      		console.log('User with socket id ' + sails.sockets.id(req) + ' is now subscribed to the model class \'user\'.');
-   		}
-  	},
+  subscribe: function (req, res) {
+    if (req.isSocket) {
+      user.watch(req);
+      console.log('User with socket id ' + sails.sockets.id(req) + ' is now subscribed to the model class \'user\'.');
+    }
+  },
 };
 
--- BetterMeetingsWithReact/api/services/QrCodeService.js
+++ BetterMeetingsWithReact/api/services/QrCodeService.js
@@ -1,14 +1,17 @@
 module.exports = {
 
   /**
-   * @param id    should be a string like '#div-id'
+   * @param src   should be a string like '/qr/code'
    * @param url   a simple url as string
    * */
-  renderQrCode: function (id, url) {
+  renderQrCode: function (url, size) {
+    var qr = require('node-qr-image');
 
-    jQuery(id).qrcode({
-      text: url
-    });
+    var qr_svg = qr.image('I love QR!', {type: 'svg'});
+    qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
 
+    var svg_string = qr.imageSync(url, {type: 'svg'});
+
+    return svg_string.replace('viewBox="0 0 27 27"', 'viewBox="0 0 27 27" width=' + size + ' height=' + size);
   }
 };
