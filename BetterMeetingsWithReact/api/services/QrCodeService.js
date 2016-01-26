dff30d10594d85c1e2d42d692f189742a49a2bd7
Benjamin Feldmann <benjamin-feldmann@web.de>
2016-01-26 14:52:10 +0100

    just forgot sth -.- :D

--- BetterMeetingsWithReact/api/services/QrCodeService.js
+++ BetterMeetingsWithReact/api/services/QrCodeService.js
@@ -12,9 +12,6 @@ module.exports = {
   renderQrCode: function (url, size) {
     var qr = require('node-qr-image');
 
-    var qr_svg = qr.image('I love QR!', {type: 'svg'});
-    qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
-
     var svg_string = qr.imageSync(url, {type: 'svg'});
 
     return svg_string.replace('viewBox="0 0 27 27"', 'viewBox="0 0 27 27" width=' + size + ' height=' + size);
