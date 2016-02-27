ac762d5453d8f4afeb80213db498f3c4b91b76b6
Matthias Radscheit <matthias@radscheit.de>
2016-02-27 18:25:51 +0100

    fixed todoFormUpdate

--- BetterMeetingsWithReact/assets/app/components/FluxTodoItemUpdateForm.react.js
+++ BetterMeetingsWithReact/assets/app/components/FluxTodoItemUpdateForm.react.js
@@ -1,7 +1,8 @@
-var React = require('react');
 var FluxMeetingActions = require('../actions/FluxMeetingActions');
 
-var InlineEdit = require('react-edit-inline');
+import InlineEdit from 'react-edit-inline';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 var FluxTodoItemUpdateForm = React.createClass({
 
@@ -53,38 +54,43 @@ var FluxTodoItemUpdateForm = React.createClass({
         var canEdit = this.props.canEdit;
         var title = (this.props.item !== null) ? this.props.item.title : null;
         var description = (this.props.item !== null) ? this.props.item.description : null;
-
-        return (
-
-            <div className="modal fade" tabIndex="-1" role="dialog" id="updateTodoItemModal">
-              <div className="modal-dialog modal-lg">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <button className="close" data-dismiss="modal"><span>×</span></button>
-                    <h4 className="modal-title">Upate todo item</h4>
-                  </div>
-                  <div className="modal-body">
-                    <div className="form-group">
-                      <label className="col-md-3 control-label">Title</label>
-                      <div className="col-md-9">
-                        <small className="help-block">Please enter a title</small>
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <label className="col-md-3 control-label">Description</label>
-                      <div className="col-md-9">
-                        <textarea className="description-textarea" rows="9" className="form-control" placeholder="Description">{description}</textarea>
-                        <small className="help-block">Please enter a description</small>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="modal-footer">
-                    <button type="submit" className="btn btn-success" onClick={this.editTodoItem}>Save changes</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-        );
+        
+                 
+        return(<div className='modal fade' tabIndex='-1' role='dialog' id='updateTodoItemModal'>
+                        <div className='modal-dialog modal-lg'>
+                          <div className='modal-content'>
+                            <div className='modal-header'>
+                              <button className='close' data-dismiss='modal'>
+                                <span> × </span> 
+                              </button>
+                              <h4 className='modal-title'>Upate todo item</h4>
+                            </div>
+                            <div className='modal-body'>
+                              <div className='form-group'>
+                                <label className='col-md-3 control-label'>Title</label>
+                                <div className='col-md-9'>
+                                  <small className='help-block'>Please enter a title</small>
+                                  <InlineEdit 
+                                    validate={this.customValidateTitle} 
+                                    text={this.state.title} 
+                                    paramName='title' 
+                                    change={this.titleChanged} />
+                                </div>
+                              </div>
+                              <div className='form-group'>
+                                <label className='col-md-3 control-label'>Description</label>
+                                <div className='col-md-9'>
+                                  <textarea className='description-textarea' rows='9' className='form-control' placeholder='Description'>{description}</textarea>
+                                  <small className='help-block'>Please enter a description</small>
+                                </div>
+                              </div>
+                            </div>
+                            <div className='modal-footer'>
+                              <button type='submit' className='btn btn-success' onClick={this.editTodoItem}>Save changes</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>);
     }
 
 });
