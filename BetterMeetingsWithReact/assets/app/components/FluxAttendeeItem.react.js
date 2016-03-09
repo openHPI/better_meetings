0900d1544c6c2ad080a9784647d528b2b065de8f
Benjamin Feldmann <benjamin-feldmann@web.de>
2016-03-09 20:35:17 +0100

    Eslint fixing

--- BetterMeetingsWithReact/assets/app/components/FluxAttendeeItem.react.js
+++ BetterMeetingsWithReact/assets/app/components/FluxAttendeeItem.react.js
@@ -3,7 +3,7 @@ var FluxMeetingActions = require('../actions/FluxMeetingActions');
 
 /**
  * FluxAttendeeList
- * 
+ *
  * @module FluxAttendeeList
  * @require React
  * @require FluxMeetingActions
@@ -11,22 +11,21 @@ var FluxMeetingActions = require('../actions/FluxMeetingActions');
  */
 var FluxAttendeeItem = React.createClass({
 
-    render: function(){
+  render: function () {
+    var attendee = this.props.attendee;
 
-        var attendee = this.props.attendee;
-
-        return (
-            <a href="#" className="list-group-item">
-                <div className="media-left">
-                    <img className="img-circle img-xs" src="/images/av2.png" alt="Profile Picture" />
-                </div>
-                <div className="media-body">
-                	<div className="text-lg">{attendee.name}</div>
-                    <span className="text-muted">{attendee.email}</span>
-                </div>
-            </a>
-        );
-    }
+    return (
+      <a href="#" className="list-group-item">
+        <div className="media-left">
+          <img className="img-circle img-xs" src="/images/av2.png" alt="Profile Picture"/>
+        </div>
+        <div className="media-body">
+          <div className="text-lg">{attendee.name}</div>
+          <span className="text-muted">{attendee.email}</span>
+        </div>
+      </a>
+    );
+  }
 });
 
 module.exports = FluxAttendeeItem;
