56437e5eaef535c2e17c4505a5210f79298e30bb
Benjamin Feldmann <benjamin-feldmann@web.de>
2016-03-10 16:38:46 +0100

    Lint FluxQrViewer

--- BetterMeetingsWithReact/assets/app/components/FluxQrViewer.react.js
+++ BetterMeetingsWithReact/assets/app/components/FluxQrViewer.react.js
@@ -1,36 +1,32 @@
 var React = require('react');
-var FluxMeetingActions = require('../actions/FluxMeetingActions');
 
-var OverlayTrigger = require('react-bootstrap').OverlayTrigger;
-var Popover = require('react-bootstrap').Popover;
-var Button = require('react-bootstrap').Button;
+require('react-bootstrap').OverlayTrigger;
+require('react-bootstrap').Popover;
 
 // Flux qrcode view
 var FluxQrViewer = React.createClass({
 
   render: function () {
-
     var qrcode = this.props.qrcode;
 
     var svgContainerStyle = {
       width: '200px',
       height: '200px'
-    }
+    };
 
     var smallSvgStyle = {
       width: '50px',
       height: '50px'
-    }
+    };
 
     return (
-
       <div className="panel">
         <div className="panel-body">
           <OverlayTrigger trigger="hover" placement="top" overlay={
                 <Popover title="Meeting Code" style={ svgContainerStyle }>
-                  <div dangerouslySetInnerHTML={{__html: qrcode}} style={ svgContainerStyle }></div>
+                  <div dangerouslySetInnerHTML={{ __html: qrcode }} style={ svgContainerStyle }></div>
                   </Popover>}>
-            <div className="center-block" dangerouslySetInnerHTML={{__html: qrcode}} style={ smallSvgStyle }></div>
+            <div className="center-block" dangerouslySetInnerHTML={{ __html: qrcode }} style={ smallSvgStyle }></div>
           </OverlayTrigger>
         </div>
       </div>
